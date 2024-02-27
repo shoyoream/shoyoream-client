bfc1c50260e74c0bd101beca96ead51a36e475de
Minsup Lee <m1nsuppp@gmail.com>
2024-02-27 23:17:16 +0900

    test: test

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="w-screen min-h-screen flex justify-center items-center">
-      w-screen min-h-screen flex justify-center items-center
+      w-screen min-h-screen flex justify-center items-center w-screen min-h-screen flex
+      justify-center items-center w-screen min-h-screen flex justify-center items-center w-screen
+      min-h-screen flex justify-center items-center
     </main>
   );
 }
