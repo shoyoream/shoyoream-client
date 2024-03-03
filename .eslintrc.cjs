969f76e3e9d3f53723bfedc6addeb366dc21a77b
Minsup Lee <m1nsuppp@gmail.com>
2024-03-03 20:33:54 +0900

    chore: 린트 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
-  extends: ["next/core-web-vitals", "turbo"],
+  extends: ['next/core-web-vitals', 'turbo'],
+  plugins: ['unicorn'],
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        caughtErrors: 'none',
+        ignoreRestSiblings: true,
+        vars: 'all',
+      },
+    ],
+    'prefer-const': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+      },
+    ],
+  },
 };
